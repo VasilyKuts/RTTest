5d20802b18aa4c8e13008862b2d7743441ce69e8
Vasily Kuts <vasily.kuts@ic-group.ru>
2025-03-10 21:03:08 +0300

    Concurrent multiple locks demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(2);
         List<ReentrantLock> lockList = new ArrayList<>();
 
